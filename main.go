7792069713d65c3d20e5b15e422c1ec6682d82c2
Ana Paula Castro <marcus.f22@gmail.com>
2022-03-30 11:43:25 -0300

    minimal code to scrape data from 'antigo.economia.gov.br' agendas

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("antigo.economia.gov.br", "www.gov.br"),
+	)
+
+	c.OnHTML("div.dados-agenda", func(e *colly.HTMLElement) {
+		fmt.Println(e.ChildText(".agenda-autoridade"))
+		fmt.Println(e.ChildText(".agenda-dia"))
+		fmt.Println()
+	})
+
+	c.OnHTML("ul.list-compromissos", func(e *colly.HTMLElement) {
+		e.ForEach("li.item-compromisso", func(_ int, el *colly.HTMLElement) {
+			fmt.Println("Horário:", el.ChildText(".comprimisso-inicio"))
+			fmt.Println("Compromisso:", el.ChildText(".comprimisso-titulo"))
+			fmt.Println("Local:", el.ChildText(".comprimisso-local"))
+			fmt.Println()
+		})
+	})
+
+	c.Visit("http://antigo.economia.gov.br/Economia/agendas/gabinete-do-ministro/ministro-da-economia/paulo-guedes/2022-03-17?month:int=3&year:int=2022")
+}
